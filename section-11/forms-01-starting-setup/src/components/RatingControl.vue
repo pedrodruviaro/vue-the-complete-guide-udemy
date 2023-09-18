825d3562800881e8c589d8c624d450d5a9be27b0
pedrodruviaro <pedrodruviaro@gmail.com>
2023-09-18 18:08:47 -0300

    add: section-11

---
+++ section-11/forms-01-starting-setup/src/components/RatingControl.vue
@@ -0,0 +1,59 @@
+<template>
+  <ul>
+    <li :class="{ active: modelValue === 'poor' }">
+      <button type="button" @click="activate('poor')">Poor</button>
+    </li>
+    <li :class="{ active: modelValue === 'average' }">
+      <button type="button" @click="activate('average')">Average</button>
+    </li>
+    <li :class="{ active: modelValue === 'great' }">
+      <button type="button" @click="activate('great')">Great</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+
+  methods: {
+    activate(option) {
+      this.$emit('update:modelValue', option);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: rgb(148, 14, 148);
+}
+
+.active button {
+  color: rgb(148, 14, 148);
+}
+
+ul {
+  list-style-type: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  padding: 1rem;
+}
+</style>
---
+++ section-11/forms-01-starting-setup/src/components/TheForm.vue
@@ -0,0 +1,203 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div class="form-control" :class="{ invalid: usernameValidity === 'invalid' }">
+      <label for="user-name">Your Name</label>
+      <input
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model.trim="username"
+        @blur="validateInput"
+      />
+      <p v-if="usernameValidity === 'invalid'">Please enter a valid name</p>
+    </div>
+    <div class="form-control">
+      <label for="age">Your Age (Years)</label>
+      <input id="age" name="age" type="number" v-model.number="age" />
+    </div>
+    <div class="form-control">
+      <label for="referrer">How did you hear about us?</label>
+      <select id="referrer" name="referrer" v-model="referrer">
+        <option value="google">Google</option>
+        <option value="wom">Word of mouth</option>
+        <option value="newspaper">Newspaper</option>
+      </select>
+    </div>
+    <div class="form-control">
+      <h2>What are you interested in?</h2>
+      <div>
+        <input id="interest-news" name="interest" v-model="interest" value="news" type="checkbox" />
+        <label for="interest-news">News</label>
+      </div>
+      <div>
+        <input
+          id="interest-tutorials"
+          name="interest"
+          v-model="interest"
+          value="tutorials"
+          type="checkbox"
+        />
+        <label for="interest-tutorials">Tutorials</label>
+      </div>
+      <div>
+        <input
+          id="interest-nothing"
+          name="interest"
+          v-model="interest"
+          value="nothing"
+          type="checkbox"
+        />
+        <label for="interest-nothing">Nothing</label>
+      </div>
+    </div>
+    <div class="form-control">
+      <h2>How do you learn?</h2>
+      <div>
+        <input id="how-video" name="how" type="radio" value="video" v-model="how" />
+        <label for="how-video">Video Courses</label>
+      </div>
+      <div>
+        <input id="how-blogs" name="how" type="radio" value="blogs" v-model="how" />
+        <label for="how-blogs">Blogs</label>
+      </div>
+      <div>
+        <input id="how-other" name="how" type="radio" value="other" v-model="how" />
+        <label for="how-other">Other</label>
+      </div>
+
+      <div class="form-control">
+        <rating-control v-model="rating"></rating-control>
+      </div>
+
+      <div class="form-control">
+        <input type="checkbox" id="confirm-terms" name="confirm-terms" v-model="confirm" />
+        <label for="confirm-terms">Confirm the terms</label>
+      </div>
+    </div>
+    <div>
+      <button>Save Data</button>
+    </div>
+  </form>
+</template>
+
+<script>
+import RatingControl from './RatingControl.vue';
+
+export default {
+  components: { RatingControl },
+  data() {
+    return {
+      username: '',
+      age: null,
+      referrer: 'wom',
+      interest: [],
+      how: null,
+      confirm: false,
+
+      rating: null,
+
+      usernameValidity: 'pending',
+    };
+  },
+
+  methods: {
+    submitForm() {
+      console.log('Username: ' + this.username);
+      console.log('Age: ' + this.age);
+      console.log('Referrer: ' + this.referrer);
+      console.log('Interest: ' + this.interest);
+      console.log('How: ' + this.how);
+      console.log('Confirm: ' + this.confirm);
+      console.log('Rating: ' + this.rating);
+
+      this.username = '';
+      this.age = null;
+      this.referrer = 'wom';
+      this.interest = [];
+      this.how = null;
+      this.confirm = false;
+      this.rating = null;
+    },
+
+    validateInput() {
+      if (this.username.trim() === '') {
+        this.usernameValidity = 'invalid';
+      } else {
+        this.usernameValidity = 'valid';
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 2rem;
+  background-color: #ffffff;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+.form-control.invalid input {
+  border: 1px solid red;
+}
+
+.form-control.invalid label {
+  color: red;
+}
+
+label {
+  font-weight: bold;
+}
+
+h2 {
+  font-size: 1rem;
+  margin: 0.5rem 0;
+}
+
+input,
+select {
+  display: block;
+  width: 100%;
+  font: inherit;
+  margin-top: 0.5rem;
+}
+
+select {
+  width: auto;
+}
+
+input[type='checkbox'],
+input[type='radio'] {
+  display: inline-block;
+  width: auto;
+  margin-right: 1rem;
+}
+
+input[type='checkbox'] + label,
+input[type='radio'] + label {
+  font-weight: normal;
+}
+
+button {
+  font: inherit;
+  border: 1px solid #0076bb;
+  background-color: #0076bb;
+  color: white;
+  cursor: pointer;
+  padding: 0.75rem 2rem;
+  border-radius: 30px;
+}
+
+button:hover,
+button:active {
+  border-color: #002350;
+  background-color: #002350;
+}
+</style>
