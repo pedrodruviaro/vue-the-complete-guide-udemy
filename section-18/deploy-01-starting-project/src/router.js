21ac469e217c75c60cad9a5dc23d239c9453d3f8
pedrodruviaro <pedrodruviaro@gmail.com>
2023-10-09 19:19:34 -0300

    add: section-18/deploy

---
+++ section-18/deploy-01-starting-project/src/router.js
@@ -0,0 +1,50 @@
+import store from './store/index.js';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachesList from './pages/coaches/CoachesList.vue';
+const CoachDetail = () => import('./pages/coaches/CoachDetail.vue');
+const CoachRegistation = () => import('./pages/coaches/CoachRegistration.vue');
+const NotFound = () => import('./pages/NotFound.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue');
+const UserAuth = () => import('./pages/auth/UserAuth.vue');
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches/:id',
+      component: CoachDetail,
+      props: true,
+      children: [
+        { path: 'contact', component: ContactCoach }, // /coaches/c1/contact
+      ],
+    },
+    {
+      path: '/register',
+      component: CoachRegistation,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/requests',
+      component: RequestsReceived,
+      meta: { requiresAuth: true },
+    },
+    { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+});
+
+router.beforeEach(function (to, _, next) {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next('/auth');
+  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+    next('/coaches');
+  } else {
+    next();
+  }
+});
+
+export default router;
